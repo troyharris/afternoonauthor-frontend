e55d58c83e39f1c9eed029d0fae598bb065747ef
Troy Harris <troyharris@users.noreply.github.com>
2018-05-22 08:33:52 -0700

    Switched to fully use Ant Design for UI. Added menu

--- src/index.js
+++ src/index.js
@@ -3,21 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Query, ApolloConsumer } from "react-apollo";
+import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import { Provider, Heading, Button, Text, Flex, Box } from "rebass";
-import { Slider } from "antd";
+import { Slider, Row, Col, Button, Layout, Menu, Icon } from "antd";
 import "antd/dist/antd.css";
+const { Header, Sider, Content } = Layout;
 
-/*
-const client = new ApolloClient({
-    uri: "http://localhost:3000/graphql"
-});
-*/
+const config = require("./config");
 
 const client = new ApolloClient({
 	//cache: new InMemoryCache(),
-	uri: "http://loneyeti.com:3000/graphql/"
+	uri: config.APIURL
 });
 
 const marks = {
@@ -34,7 +30,7 @@ const topMarks = {
 
 function formatter(valueStr) {
 	const value = Number(valueStr);
-	if (value < 50 && value != 100) {
+	if (value < 50 && value !== 100) {
 		return "All";
 	}
 	if (value === 100) {
@@ -87,22 +83,18 @@ class RandomName extends React.Component {
 					const name = data.name[0];
 					const results = (
 						<div>
-							<Text
-								fontSize={7}
-								textAlign="center"
-								fontWeight="bold"
-								bg="gray"
-								p={3}
-							>
+							<h1>
 								{name.firstName} {name.surname}
-							</Text>
-							<button
+							</h1>
+							<Button
+								type="primary"
+								size="large"
+								shape="circle"
+								icon="reload"
 								onClick={() => {
 									refetch(this.props.refresh());
 								}}
-							>
-								Reload
-							</button>
+							/>
 						</div>
 					);
 
@@ -116,16 +108,18 @@ class RandomName extends React.Component {
 class GenderSlider extends React.Component {
 	render() {
 		return (
-			<Box width={1 / 2} p={3}>
-				<Text>Gender</Text>
-				<Slider
-					tipFormatter={formatter}
-					marks={marks}
-					step={null}
-					defaultValue={0}
-					onChange={value => this.props.onChange(value)}
-				/>
-			</Box>
+			<Col xs={24} sm={12} md={12} lg={12} xl={12}>
+				<div className="slider">
+					<p>Gender</p>
+					<Slider
+						tipFormatter={formatter}
+						marks={marks}
+						step={null}
+						defaultValue={0}
+						onChange={value => this.props.onChange(value)}
+					/>
+				</div>
+			</Col>
 		);
 	}
 }
@@ -133,15 +127,17 @@ class GenderSlider extends React.Component {
 class TopSlider extends React.Component {
 	render() {
 		return (
-			<Box width={1 / 2} p={3}>
-				<Text>Top</Text>
-				<Slider
-					defaultValue={100}
-					marks={topMarks}
-					tipFormatter={topFormatter}
-					onChange={value => this.props.onChange(value)}
-				/>
-			</Box>
+			<Col xs={24} sm={12} md={12} lg={12} xl={12}>
+				<div className="slider">
+					<p>Top</p>
+					<Slider
+						defaultValue={100}
+						marks={topMarks}
+						tipFormatter={topFormatter}
+						onChange={value => this.props.onChange(value)}
+					/>
+				</div>
+			</Col>
 		);
 	}
 }
@@ -154,9 +150,15 @@ class App extends React.Component {
 			currentTop: null,
 			sendGender: null,
 			sendTop: null,
-			refresh: 0
+			refresh: 0,
+			collapsed: false
 		};
 	}
+	toggle = () => {
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	};
 	handleGenderChange(gender) {
 		let genderStr = null;
 		if (gender === 50) {
@@ -183,33 +185,60 @@ class App extends React.Component {
 	}
 	render() {
 		return (
-			<div>
-				<Provider>
-					<Box width={1 / 2} mx="auto" mt={100}>
-						<Heading>Random Name</Heading>
-						<Flex>
-							<GenderSlider
-								onChange={gender => this.handleGenderChange(gender)}
-							/>
-							<TopSlider onChange={top => this.handleTopChange(top)} />
-						</Flex>
-						<ApolloProvider client={client}>
-							<RandomName
-								gender={this.state.currentGender}
-								top={this.state.currentTop}
-								refresh={() => this.onRefresh()}
-							/>
-						</ApolloProvider>
-					</Box>
-				</Provider>
-			</div>
+			<Layout className="container">
+				<Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+					<div className="logo" />
+					<Menu theme="dark" mode="inline" defaultSelectedKeys={["2"]}>
+						<Menu.Item key="1">
+							<Icon type="home" />
+							<span>About</span>
+						</Menu.Item>
+						<Menu.Item key="2">
+							<Icon type="user" />
+							<span>Random Name</span>
+						</Menu.Item>
+					</Menu>
+				</Sider>
+				<Layout>
+					<Header style={{ background: "#fff", padding: "0 0 0 24px" }}>
+						<Icon
+							className="trigger"
+							type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
+							onClick={this.toggle}
+							style={{ fontSize: 16 }}
+						/>
+					</Header>
+					<Content
+						style={{
+							margin: "24px 16px",
+							padding: 24,
+							background: "#fff",
+							minHeight: 280
+						}}
+					>
+						<Row type="flex" justify="center">
+							<Col xs={24} sm={18} md={16} lg={14} xl={12}>
+								<h1>Random Name</h1>
+								<Row type="flex" justify="center">
+									<GenderSlider
+										onChange={gender => this.handleGenderChange(gender)}
+									/>
+									<TopSlider onChange={top => this.handleTopChange(top)} />
+								</Row>
+								<ApolloProvider client={client}>
+									<RandomName
+										gender={this.state.currentGender}
+										top={this.state.currentTop}
+										refresh={() => this.onRefresh()}
+									/>
+								</ApolloProvider>
+							</Col>
+						</Row>
+					</Content>
+				</Layout>
+			</Layout>
 		);
 	}
 }
 
-ReactDOM.render(
-	<div>
-		<App />
-	</div>,
-	document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
